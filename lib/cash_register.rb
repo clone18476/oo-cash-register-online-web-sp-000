94adab6434734126d737b1633e7e0d207292f0d9
Cameron Cousins <cameroncousins18@gmail.com>
2020-09-11 20:31:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -34,7 +34,10 @@ class CashRegister
   end
 
   def void_last_transaction
-   self.total = @total - @transactions.pop
-   self.total = 0.0
+    if 
+      self.total = @total - @transactions.pop
+    else 
+      self.total = 0.0
+    end 
   end
 end
